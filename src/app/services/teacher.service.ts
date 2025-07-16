2ef9a5a8dba89de8100dd2b3eb4f8a6cdbc778ae
dembouz07 <dembouzfaye7@gmail.com>
2025-07-16 22:00:18 +0000

    committest

--- src/app/services/teacher.service.ts
+++ src/app/services/teacher.service.ts
@@ -8,7 +8,7 @@ import { HttpHeaders } from '@angular/common/http';
 export class TeacherService {
   //private URL = 'http://172.20.10.5:8000/api/v1/teachers';
 
-  private URL = "https://e24585d334ce.ngrok-free.app/api/v1/teachers";
+  private URL = "https://18f72cfb8d9a.ngrok-free.app/api/v1/teachers";
 
 
   constructor(private http: HttpClient) {}
